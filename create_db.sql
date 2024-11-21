5ecd66085b3d336ec6864db87d39ca137a7dbecc
wxjiha <120654528+wxjiha@users.noreply.github.com>
2024-11-21 13:36:39 +0000

    error editing

--- create_db.sql
+++ create_db.sql
@@ -5,7 +5,7 @@ CREATE DATABASE IF NOT EXISTS webapp;
 USE webapp;
 
 # Create the tables
-CREATE TABLE IF NOT EXISTS books (id INT AUTO_INCREMENT,name VARCHAR(50),price DECIMAL(5, 2) unsigned,PRIMARY KEY(id));
+CREATE TABLE IF NOT EXISTS items (id INT AUTO_INCREMENT,name VARCHAR(50),price DECIMAL(5, 2) unsigned,PRIMARY KEY(id));
 
 # Create the app user
 CREATE USER IF NOT EXISTS 'app_user'@'localhost' IDENTIFIED BY 'qwerty'; 

95186454784e28f72af1e8656a2eb85da8aa990f
wxjiha <120654528+wxjiha@users.noreply.github.com>
2024-12-09 00:18:23 +0000

    edits pt3

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,7 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const { check, validationResult } = require('express-validator');
 
+
 const redirectLogin = (req, res, next) => {
     if (!req.session.userId ) {
       res.redirect('./login') // redirect to the login page
@@ -25,7 +26,7 @@ router.post('/loggedin',
     [check('password').notEmpty().withMessage('Password is required')],
     function (req, res, next) {
     req.sanitize(req.body.username)
-    req.sanitize(req.body.password)
+    req.sanitize(req.body.plainPassword)
     const { username, password } = req.body;
 
     // Query the database to find the user by username
@@ -72,14 +73,14 @@ router.get('/register', function (req, res, next) {
 })    
 
 router.post('/registered', 
-    [check('username').notEmpty().withMessage('Username is required').isLength({ min: 3, max: 20 }).withMessage('Username must be between 3 and 20 characters')]
+    [check('username').notEmpty().withMessage('Username is required').isLength({ min: 3, max: 20 }).withMessage('Username must be between 3 and 20 characters')],
     [check('email').isEmail()], 
     [check('password').isLength({min:8}).withMessage('Password must be at least 8 characters long')], 
     [check('confirmPassword').custom((value, { req }) => value === req.body.password).withMessage('Passwords do not match')],
     function (req, res, next) {
     req.sanitize(req.body.username)
     req.sanitize(req.body.email)
-    req.sanitize(req.body.password)
+    req.sanitize(req.body.plainPassword)
     const errors = validationResult(req);
         if (!errors.isEmpty()) {
             res.redirect('./register'); }
@@ -115,34 +116,34 @@ router.post('/registered',
         }
 });
 
-// // Route to display the list of users 
-// router.get('/users/userlist', function (req, res, next) {
-//     // SQL query to fetch user details excluding hashed passwords
-//     const sql = "SELECT username, email FROM users"
+//// // Route to display the list of users 
+//// router.get('/users/userlist', function (req, res, next) {
+////     // SQL query to fetch user details excluding hashed passwords
+//  //   const sql = "SELECT username, email FROM users"
 
-//     // Execute the query
-//     db.query(sql, (err, results) => {
-//         if (err) {
-//             next(err)
-//         }
+//    // // Execute the query
+//    // db.query(sql, (err, results) => {
+//      //   if (err) {
+//        //     next(err)
+//       //  }
+//
+////         // Render the users list page with the fetched data
+//  //       res.render('userlist.ejs', { users: results });
+//    // });
+//// });
 
-//         // Render the users list page with the fetched data
-//         res.render('userlist.ejs', { users: results });
-//     });
-// });
+// //// Define the /list route
+// //router.get('/userslist', redirectLogin, function (req, res) {
+//   //  let sql = 'SELECT username, email FROM users';
 
-// // Define the /list route
-// router.get('/userslist', redirectLogin, function (req, res) {
-//     let sql = 'SELECT username, email FROM users';
+//  //   db.query(sqlquery, (err, results) => {
+//    //     if (err) {
+//      //       throw err
+//        // }
 
-//     db.query(sqlquery, (err, results) => {
-//         if (err) {
-//             throw err
-//         }
-
-//         res.render('userslist.ejs', { userslist,result});
-//     });
-// });
+//      //   res.render('userslist.ejs', { userslist,result});
+//    // });
+//// });
 
 // Export the router object so index.js can access it
 module.exports = router;

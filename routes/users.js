e248ab39ad813dd5a1ff8c489ac4ccd14781cd4e
wxjiha <120654528+wxjiha@users.noreply.github.com>
2024-12-13 00:40:31 +0000

    fixing pt3

--- routes/users.js
+++ routes/users.js
@@ -65,7 +65,7 @@ router.get('/logout', redirectLogin,(req, res) => {
     req.session.destroy((err) => {
       if (err) {
         console.error(err);
-        return res.redirect('/usr/448/');
+        return res.redirect('/users/login');
       }
       res.render('logout', {message:"You've successfully logged out, you are being redirected to the home page"}); 
     });
